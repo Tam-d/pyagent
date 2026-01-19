678cf10a15032d0b2c7d91fce47c609b99f31562
Tam <5542463+Tam-d@users.noreply.github.com>
2026-01-19 16:17:24 +0000

    CH2L2: add get files

---
+++ functions/get_files_info.py
@@ -0,0 +1,45 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    
+    work_path_abs = os.path.abspath(working_directory)
+    joined_path = os.path.join(work_path_abs, directory)
+    target_path = os.path.normpath(joined_path)
+    valid_target = os.path.commonpath([work_path_abs, target_path]) == work_path_abs
+
+    if not valid_target:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    directory_is_dir = os.path.isdir(target_path)
+
+    if not directory_is_dir:
+        return f'Error: "{directory}" is not a directory'
+
+    # print(f"Directory is dir?: {directory_is_dir}")
+    # print(f"Path: {work_path_abs}")
+    # print(f"Joined: {joined_path}")
+    # print(f"Target: {target_path}")
+    # print(f"Valid Target: {valid_target}")
+
+    dir_info = ""
+
+    for item in os.listdir(target_path):
+        try:
+            item_path = os.path.join(target_path, item)
+            item_is_dir = os.path.isdir(item_path)
+            item_size = os.path.getsize(item_path)
+
+            item_info = f"- {item}: file_size={item_size} bytes, is_dir={item_is_dir}"
+            dir_info += f"{item_info}\n"
+
+        except Exception as e:
+            return f"Error: {e}"
+    
+    return dir_info
+
+def main():
+
+    get_files_info("calculator")
+
+
+main()
