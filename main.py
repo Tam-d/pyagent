7c0506b20d907a41c704bcc12c93f7e6d1c39ec0
Tam <5542463+Tam-d@users.noreply.github.com>
2026-01-20 16:38:34 +0000

    CH3L4: calling functions via llm

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 def main():
     load_dotenv()
@@ -53,9 +53,31 @@ def main():
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
 
+    function_results = []
+
     if function_calls:
         for function_call in function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
+            # print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call)
+
+            function_parts = function_call_result.parts
+            
+            if not function_call_result.parts or len(function_parts) == 0:
+                raise Exception("Function call result did not have any parts")
+            
+            function_part = function_call_result.parts[0]
+            function_response = function_part.function_response
+
+            if not function_response:
+                raise Exception("Function call parts did not have a function response")
+            
+            if not function_response.response:
+                raise Exception("The functions response was empty")
+            
+            function_results.append(function_part)
+
+            if args.verbose == True:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
     else:
         print(f"Response:\n {response.text}")
 
